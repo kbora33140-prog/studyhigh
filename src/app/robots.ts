449bb8741d0e9b1f7d2882f6847ead66a9fc7c5d
kbora33140-prog <kbora33140@gmail.com>
2026-07-15 20:26:18 +0900

    fix: update robots sitemap URL

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://studyhigh.example.com/sitemap.xml",
+    sitemap: "https://studyhigh.co.kr/sitemap.xml",
   };
 }
